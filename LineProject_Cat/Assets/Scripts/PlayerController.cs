158db7de54cc2e0c9e2c968c8bf6038842dd58a8
SonSang <shh1295@gmail.com>
2016-07-20 11:36:08 +0900

    Fixed PlayerController

--- LineProject_Cat/Assets/Scripts/PlayerController.cs
+++ LineProject_Cat/Assets/Scripts/PlayerController.cs
@@ -40,7 +40,7 @@ public class PlayerController : MonoBehaviour
 
         moveVelocity = 0;
 
-        /*if(PlayerPrefs.GetString("HorizontalDirection") == "Right")
+        if(PlayerPrefs.GetString("HorizontalDirection") == "Right")
         {
             moveVelocity = moveSpeed;
         }
@@ -53,24 +53,6 @@ public class PlayerController : MonoBehaviour
         rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
 
 		if (PlayerPrefs.GetString("Jump") == "Jump" && isGrounded)
-        {
-            anim.SetBool("Jumping", true);
-            jump = true;
-        }*/
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            moveVelocity = moveSpeed;
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            moveVelocity = (-1) * moveSpeed;
-        }
-
-        rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
-
-        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             anim.SetBool("Jumping", true);
             jump = true;
