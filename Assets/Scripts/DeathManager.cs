0c2e0a81b00c5326b9594b14195e07ccd9ddd358
SonSang <shh1295@gmail.com>
2016-07-31 11:24:09 +0900

    Added stage bgm

--- Assets/Scripts/DeathManager.cs
+++ Assets/Scripts/DeathManager.cs
@@ -20,18 +20,5 @@ public class DeathManager : MonoBehaviour {
         GameObject c = Instantiate(corpse, player.transform.position, player.transform.rotation) as GameObject;
 
         c.transform.localScale = player.transform.localScale;
-
-        // if player was killed by obstacle
-        if (kp as ObstacleManager != null)
-        {
-            ObstacleManager obstacle = kp as ObstacleManager;
-            c.transform.parent = obstacle.transform;
-            Vector3 corpseLP = c.transform.localPosition;
-            c.transform.localPosition = new Vector3(corpseLP.x, obstacle.corpseOffset, corpseLP.z);
-        }
-        // if player was killed by moving enemy
-        else if(kp as EnemyController != null)
-        {
-        }
     }
 }
