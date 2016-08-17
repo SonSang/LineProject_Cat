08f55b85ecb0bd635f7ccde56b555ae14b8bdb8d
SonSang <shh1295@gmail.com>
2016-08-17 14:09:04 +0900

    Changed death horizon mechanism

--- Assets/Scripts/DeathHorizonManager.cs
+++ Assets/Scripts/DeathHorizonManager.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class DeathHorizonManager : KillPlayer {
 
-    public float respawnHeight;
-
 	// Use this for initialization
 	void Start () {
         levelManager = FindObjectOfType<LevelManager>();
--- Assets/Scripts/DeathManager.cs
+++ Assets/Scripts/DeathManager.cs
@@ -3,20 +3,35 @@ using System.Collections;
 
 public class DeathManager : MonoBehaviour {
 
+    private GameObject[] edgeSpot;
+    private float respawnHeight;
+
     private GameObject corpse;
+
+    void Start()
+    {
+        edgeSpot = GameObject.FindGameObjectsWithTag("EdgeSpot");
+        SortEdgeSpot();
+    }
 
     // Create corpse when player die
 	public float makeCorpse(PlayerController player, KillPlayer kp)
     {
         corpse = player.corpse;
 
-        DeathHorizonManager horizon = kp as DeathHorizonManager;
-
-        if (horizon != null)
+        if (kp as DeathHorizonManager != null)
         {
-            Vector3 corpsePos = new Vector3(player.transform.position.x, horizon.respawnHeight, player.transform.position.z);
+            for(int i = 0; i < edgeSpot.Length; i++)
+            {
+                if(edgeSpot[i].transform.position.x < player.transform.position.x && edgeSpot[i+1].transform.position.x > player.transform.position.x)
+                {
+                    respawnHeight = (edgeSpot[i].transform.position.y + edgeSpot[i + 1].transform.position.y) / 2 - 0.5f;
+                    break;
+                }
+            }
+            Vector3 corpsePos = new Vector3(player.transform.position.x, respawnHeight, player.transform.position.z);
             (Instantiate(corpse, corpsePos, player.transform.rotation) as GameObject).transform.localScale = player.transform.localScale;
-            return horizon.respawnHeight;
+            return respawnHeight;
         }
 
         GameObject c = Instantiate(corpse, player.transform.position, player.transform.rotation) as GameObject;
@@ -24,5 +39,23 @@ public class DeathManager : MonoBehaviour {
         c.transform.localScale = player.transform.localScale;
 
         return c.transform.position.y;
+    }
+
+    void SortEdgeSpot()
+    {
+        Debug.Log("Start sorgin");
+        for(int j = 1; j < edgeSpot.Length; j++)
+        {
+            for (int i = 0; i < edgeSpot.Length - j; i++)
+            {
+                if (edgeSpot[i].transform.position.x > edgeSpot[i + 1].transform.position.x)
+                {
+                    Debug.Log("Change");
+                    GameObject temp = edgeSpot[i];
+                    edgeSpot[i] = edgeSpot[i + 1];
+                    edgeSpot[i + 1] = temp;
+                }
+            }
+        }
     }
 }
