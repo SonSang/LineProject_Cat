8b320a46c776a9ded7a4c3dab4934462b5da227d
EQ1 <bluetwintail@naver.com>
2016-08-27 17:05:32 +0900

    Make ClairvoCat and BambooCat

---
+++ Assets/Scripts/CatSkill/BambooHelicopter_BambooCat.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BambooHelicopter_BambooCat : MonoBehaviour
+{
+    public float HoveringTime;
+
+    private bool IsHovering;
+    private float StartHoveringTime;
+    private float NowTime;
+
+    void Start()
+    {
+        IsHovering = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && !IsHovering)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 400));
+            GetComponent<Rigidbody2D>().gravityScale = 0.8f;
+            StartHoveringTime = Time.time;
+            IsHovering = true;
+        }
+
+        if (IsHovering)
+        {
+            NowTime = Time.time;
+
+            if (NowTime - StartHoveringTime > HoveringTime)
+            {
+                IsHovering = false;
+                GetComponent<Rigidbody2D>().gravityScale = 3f;
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 2);
+        }
+    }
+}
