25316faecfcfceb9868de7e0dd123f6b9ba9b055
EQ1 <bluetwintail@naver.com>
2016-08-29 10:18:05 +0900

    Try to Make Schrodinger Cat

---
+++ Assets/Scripts/CatSkill/ObstacleCheck_SchrodingerCat.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleCheck_SchrodingerCat : MonoBehaviour
+{
+    public bool ObstacleCheck;
+
+    void Awake()
+    {
+        ObstacleCheck = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        ObstacleCheck = true;
+        Debug.Log("Something!");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        ObstacleCheck = false;
+        Debug.Log("Something Out!");
+    }
+}
---
+++ Assets/Scripts/CatSkill/PassWall_SchrodingerCat.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PassWall_SchrodingerCat : MonoBehaviour
+{
+    public bool SchrodingerMode;
+    public GameObject ObstacleChecker;
+
+    private bool IsGround;
+    private bool ObstacleCheck;
+
+    void Start()
+    {
+        SchrodingerMode = false;
+        ObstacleCheck = false;
+    }
+
+    void Update()
+    {
+        IsGround = GetComponent<PlayerController>().isGrounded;
+        Debug.Log(ObstacleCheck);
+        Debug.Log(GetComponent<ObstacleCheck_SchrodingerCat>().ObstacleCheck);
+        ObstacleCheck = GetComponent<ObstacleCheck_SchrodingerCat>().ObstacleCheck;
+
+        if (IsGround)
+        {
+            if (Input.GetKeyDown(KeyCode.Z) && !SchrodingerMode)
+            {
+                Debug.Log("SchrodingerMode On!");
+                SchrodingerMode = true;
+                GetComponent<Rigidbody2D>().gravityScale = 0;
+                GetComponent<Collider2D>().enabled = false;
+                return;
+            }
+        }
+
+        if (SchrodingerMode)
+        {
+            if (ObstacleCheck)
+            {
+                Debug.Log("Percentage Down");
+            }
+
+            if (!IsGround)
+            {
+                Debug.Log("SchrodingerMode Off!");
+                SchrodingerMode = false;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                Debug.Log("SchrodingerMode Off!");
+                SchrodingerMode = false;
+            }
+        }
+
+        if (!SchrodingerMode)
+        {
+            GetComponent<Rigidbody2D>().gravityScale = 3;
+            GetComponent<Collider2D>().enabled = true;
+        }
+    }
+}
