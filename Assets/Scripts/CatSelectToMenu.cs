3af0eed48ba10c3d2715edf4944e3a0e0f067fda
EQwon <bluetwintail@naver.com>
2016-08-30 21:55:38 +0900

    메뉴 버튼으로 돌아가는 버튼 만들어보려고함

---
+++ Assets/Scripts/CatSelectToMenu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CatSelectToMenu : MonoBehaviour
+{
+	public void GoToMenu()
+	{
+		Debug.Log ("Clicked");
+		SceneManager.LoadScene ("Title Menu");
+	}
+}
