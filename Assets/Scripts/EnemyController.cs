713e49de7d3d627f2f4b5ab82fefec8cbd8e37f3
qria <frigen@naver.com>
2016-08-30 14:56:39 +0900

    Add image and refactor enemyController

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,32 +3,31 @@ using System.Collections;
 
 public class EnemyController : KillPlayer, FindPlayerInterface {
 
+    // Public variables settable in editor
     private float moveSpeed;
-    public bool isRight;
+    public float attackDistance; // Aggro range
+    public float attackInertiaDistance; // 
+    
+    public Transform groundCheck; // Point where ai checks for ground
+    public Transform wallCheck; // Point where ai checks for wall
 
-    public float moveRange;
-    public bool isAttacking;
-    public int attackRange;
-    public int attackOffset;
-
-    public Transform groundCheck;
-    public Transform wallCheck;
     public float groundCheckRadius;
     public float wallCheckRadius;
-    public bool isGroundedAhead;
-    public bool isWallAhead;
-    public LayerMask whatIsGround;
+
+    /// Note you also need to set the "LeftPoint" and "RightPoint" in the editor.
+    private Vector3 patrolLeftPoint; // Edit position of "LeftPoint"
+    private Vector3 patrolRightPoint; // Edit position of "RightPoint"
+
+    // private variables
 
     private Rigidbody2D rb2d;
     private Animator anim;
     private PlayerController player;
 
+    private bool isRight;
+    private bool isAttacking;
+
     private bool enteringPatrol;
-    private bool atPatrolEdge;
-    private float patrolPoint;
-    private float patrolLeftPoint;
-    private float patrolRightPoint;
-    private float turningPoint;
 
 	// Use this for initialization
 	void Start () {
@@ -36,48 +35,46 @@ public class EnemyController : KillPlayer, FindPlayerInterface {
         anim = GetComponent<Animator>();
         player = FindObjectOfType<PlayerController>();
         levelManager = FindObjectOfType<LevelManager>();
-        moveSpeed = (float)(FindObjectOfType<PlayerController>().moveSpeed * 0.8);
+        moveSpeed = player.moveSpeed * 0.8f;
         enteringPatrol = true;
-        atPatrolEdge = false;
+	    isRight = true;
 
-        patrolPoint = transform.position.x;
-        patrolLeftPoint = transform.position.x - (moveRange * (float)0.5);
-        patrolRightPoint = transform.position.x + (moveRange * (float)0.5);
+        // Set patrol range with "LeftPoint" "RightPoint"
+        Transform leftPoint = transform.FindChild("LeftPoint");
+	    Transform rightPoint = transform.FindChild("RightPoint");
+        leftPoint.gameObject.SetActive(false);  
+        rightPoint.gameObject.SetActive(false);
+
+
+        patrolLeftPoint = leftPoint.position;
+        patrolRightPoint = rightPoint.position;
     }
 
     public void FindPlayer()
     {
+        // Reload player when player respawns since the reference changes.
         player = FindObjectOfType<PlayerController>();
     }
-
-    float CheckPlayer()
-    {
-        return (transform.position.x - player.transform.position.x);
-    }
-
-    float CheckPatrolPoint()
-    {
-        return (transform.position.x - patrolPoint);
-    }
+    
 
     void Attack()
     {
         anim.SetBool("Attack", true);
 
-        if (CheckPlayer() < -attackOffset) // player is on the right side
+        if (transform.position.x - player.transform.position.x < -attackInertiaDistance) // player is on the right side
         {
             if (!isRight)
                 Flip();
             rb2d.velocity = new Vector2(moveSpeed, rb2d.velocity.y);
         }
-        else if (CheckPlayer() > attackOffset) // player is on the left side
+        else if (transform.position.x - player.transform.position.x > attackInertiaDistance) // player is on the left side
         {
             if (isRight)
                 Flip();
             rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
         }
 
-        if (!isGroundedAhead || isWallAhead)
+        if (!isGroundAhead() || isWallAhead())
             rb2d.velocity = new Vector2(0, rb2d.velocity.y);
     }
 
@@ -90,26 +87,34 @@ public class EnemyController : KillPlayer, FindPlayerInterface {
         else
             rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
 
-        if (transform.position.x <= patrolLeftPoint && !isRight)
+        bool atPatrolEdge;
+        if (transform.position.x <= patrolLeftPoint.x && !isRight)
             atPatrolEdge = true;
-        else if (transform.position.x >= patrolRightPoint && isRight)
+        else if (transform.position.x >= patrolRightPoint.x && isRight)
             atPatrolEdge = true;
         else
             atPatrolEdge = false;
 
-        if (!isGroundedAhead || isWallAhead || atPatrolEdge)
+        if (!isGroundAhead() || isWallAhead() || atPatrolEdge)
             Flip();
     }
-	
+
+    bool isGroundAhead()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, LayerMask.GetMask("Ground"));
+    }
+
+    bool isWallAhead()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, groundCheckRadius, LayerMask.GetMask("Ground"));
+    }
 	// Update is called once per frame
 	void Update () {
-
-        isGroundedAhead = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
-        isWallAhead = Physics2D.OverlapCircle(wallCheck.position, groundCheckRadius, whatIsGround);
-
+        
         if (!levelManager.IsPlayerDead)
         {
-            if (Mathf.Abs(CheckPlayer()) < attackRange && player.name != "BabyCat")
+            if (Mathf.Abs(transform.position.x - player.transform.position.x) < attackDistance 
+                && player.name != "BabyCat")
                 isAttacking = true;
             else
                 isAttacking = false;
@@ -137,13 +142,13 @@ public class EnemyController : KillPlayer, FindPlayerInterface {
 
     void ReturnToPatrolPoint()
     {
-        if (CheckPatrolPoint() > 0.5)
+        if (transform.position.x - patrolRightPoint.x > 0.5)
         {
             if (isRight)
                 Flip();
             rb2d.velocity = new Vector2(-moveSpeed, rb2d.velocity.y);
         }
-        else if (CheckPatrolPoint() < -0.5)
+        else if (transform.position.x - patrolLeftPoint.x < -0.5)
         {
             if (!isRight)
                 Flip();
