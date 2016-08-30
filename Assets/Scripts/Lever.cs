3310bdfecb31c51bc9825cf23be84b1106b1f551
AirHorizons <guardian1248@snu.ac.kr>
2016-08-30 20:00:47 +0900

    add catfood on stage1-4

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -19,6 +19,8 @@ public class Lever : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
+        animator = GetComponent<Animator>();
+
         switch (leverType)
         {
             case LeverType.MovePlatform:
@@ -51,15 +53,16 @@ public class Lever : MonoBehaviour {
                 break;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("switch", isOn);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        isOn = true;
+        isOn = (isOn) ? false : true;
         switch (leverType)
         {
             case LeverType.MovePlatform:               
